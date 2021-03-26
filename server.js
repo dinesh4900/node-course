81b6c75b559701bc44836145c6c460ef1a0c4b7a
dinesh4900 <dineshm4900@gmail.com>
2021-03-26 19:27:58 +0530

    changes in server js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     console.log(num);
 
     const greet = _.once(() => {
-        console.log('hello');
+        console.log('heyy');
     });
 
     greet();
