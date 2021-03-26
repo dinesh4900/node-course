6bc472496056fcbdc7554ccbf706367241cc636d
dinesh4900 <dineshm4900@gmail.com>
2021-03-26 10:37:35 +0530

    nodemon

--- server.js
+++ server.js
@@ -14,13 +14,22 @@ const server = http.createServer((req, res) => {
     switch(req.url){
         case '/':
             path = path+'index.html';
-
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            // it is used for redirecting 301 code is for redirect where range of 300 is used for redirecting
+            // 1st argument is Location and second is the page which we are reusing to redirect
+            res.statusCode = 301;
+            res.setHeader('Location', './about');
+            res.end();
             break;
         default:
             path += '404.html'
+            res.statusCode = 404;
             break;
     }
 
@@ -30,6 +39,7 @@ const server = http.createServer((req, res) => {
             res.end();
         }else{
             //res.write(data);  // if we area sending only one data as response we can ignore this line
+            
             res.end(data);
         }
     })
@@ -39,6 +49,6 @@ const server = http.createServer((req, res) => {
 
 // listening to the server
 server.listen(3000, 'localhost', () => {
-    console.log('lfor requests on port 3000');
+    console.log('waiting for requests on port 3000');
 });
 
