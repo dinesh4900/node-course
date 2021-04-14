e79a852467b12dcd2a92930319700781253c58a3
dinesh4900 <dineshm4900@gmail.com>
2021-04-14 20:07:18 +0530

    ejs view engines

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engines
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews')  //setting view path
+
 // listent for requests
-app.listen(3001);
+app.listen(3000);
 console.log("listening");
 
 // respond to req
@@ -13,23 +17,33 @@ app.get('/', (req, res)=>{
     // no need to define status code and content type
     //res.send('<p> hello world </p>');
     // root: __dirname indicates we should give the directory where it is present besacuse it takes it as the absolute path
-    res.sendFile('./views/index.html', { root: __dirname})
+    // res.sendFile('./views/index.html', { root: __dirname});
+
+    res.render('index');
 });
 
 app.get('/about', (req, res)=>{
     // no need to define status code and content type
     // res.send('<p> hey hello world </p>');
-    res.sendFile('./views/about.html', { root: __dirname})
+    // res.sendFile('./views/about.html', { root: __dirname});
+
+    res.render('about');
 });
 
-// redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-    console.log("hello world");
+app.get('/blogs/create', (req, res) =>{
+    res.render('create');
 })
+
+// redirect
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+//     console.log("hello world");
+// })
 
 // 404 -- it should be at the bottom of the code because if it is in first it acts as default
  app.use((req, res) => {
      // express doesnt recognise it is a 404 error so we should give status code to the line
-    res.status(404).sendFile('./views/404.html', { root: __dirname});
+     // res.status(404).sendFile('./views/404.html', { root: __dirname});
+
+     res.status(404).render('404');
 });
